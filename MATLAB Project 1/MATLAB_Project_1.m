2e73eb49cfb8fcf07fed8824a26b3852c8feae0a
josephchang98 <32658356+josephchang98@users.noreply.github.com>
2019-04-11 17:11:30 -0500

    fixed horizontal scaling; added re-scaling after conv; added time arrays for previous steps

--- MATLAB Project 1/MATLAB_Project_1.m
+++ MATLAB Project 1/MATLAB_Project_1.m
@@ -9,6 +9,7 @@ F_1F = 1e9; % Intermediate Frequency
 T_p = 0.5e-6; % End of time vector
 B = 100e6; % Bandwidth
 P = 1; % Power
+A = sqrt(2); % Amplitude = sqrt(2*Power)
 R = 1000; % Distance
 c = 3e8; % The Speed of Light
 F_c = 8e9; % Center Frequency
@@ -18,12 +19,12 @@ upconverting_freq = 7e9; % Frequency of the cosine
 sflag = 20e9; % Sampling
 Pn = 10; % Noise power
 sampling_rate = 20e9;
-F_s = 1 ./ sampling_rate; % Sampling rate
+F_s = 1 ./ sampling_rate; % Period
 
 %%% Background
 
 % Time array
-t = 0:F_s: T_p;
+t = 0:F_s:T_p;
 
 % Chirp rate
 gamma = B ./ T_p;
@@ -32,7 +33,7 @@ gamma = B ./ T_p;
 a = rectangularPulse(0,T_p,t);
 
 % x_1F
-x_1F = a .* cos((2 .* pi .* (F_1F - (0.5 .* B)) .* t) + (pi .* gamma .* t .* t));
+x_1F = A .* a .* cos((2 .* pi .* (F_1F - (0.5 .* B)) .* t) + (pi .* gamma .* t .* t));
 
 % x(t) 
 x = a .* cos(((2 .* pi) .* ((2 .* pi) .* (F_c - (0.5 .* B))) .* t) + (pi .* gamma .* t .* t));
@@ -50,8 +51,13 @@ g = alpha .* a .* cos((2 .* pi .* F_c .* t) - (2 .* pi * F_c .* tau) - (2 .* pi 
 upconvert = cos(2 .* pi .* upconverting_freq .* t);
 x_1F_upconverted = upconvert .* x_1F;
 F = -1e14:20e9:1e14;
+F_f = linspace(-1e10, 1e10, length(F))
 figure(1)
-plot(F,fftshift(abs(fft(x_1F_upconverted))))
+plot(F_f,fftshift(abs(fft(x_1F_upconverted))))
+
+
+figure(2)
+plot(F_f,fftshift(abs(fft(x_1F))))
 
 % Bandpass filter
 T_1 = -250e-9; % Not sure about this
@@ -61,13 +67,20 @@ W = 4 .* B;
 w = W .* transpose(hamming(length(t_BPF)));
 h = w .* sinc(W .* t_BPF);
 h_BPF = h .* cos(2 .* pi .* F_c .* t_BPF);
-figure(2)
-plot(F,fftshift(abs(fft(h_BPF))))
+figure(3)
+plot(F_f,fftshift(abs(fft(h_BPF))))
 
 % Convolution
 x_BPF = conv(h_BPF,x_1F_upconverted);
+figure(4)
+F_conv = -2e14:20e9:2e14;
+F_f_conv = linspace(-2e10, 2e10, length(F_conv))
+plot(F_f_conv, fftshift(abs(x_BPF)))
+
+t_conv = 0:F_s:2*T_p; % time array for x_BPF; if you plot it, it is centered at 0.5...delay?
 
 % Scaling and Truncation -- a problem for future allyson lmao
+x_BPF = (sqrt(2)./max(x_BPF)) .* x_BPF;
 
 
 % New Time Arrays
